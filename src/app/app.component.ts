2f6caedb029a0707d64d736d2af4e8f361ced399
Mikael Jonsson <mikael.jonsson@sogeti.se>
2023-10-13 15:52:47 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fetch-data-ang';
+  data: any[] = [];
+  showData = false;
+  shipName = '';
+  shipCapacity = '';
+
+  constructor(private http: HttpClient) {}
+
+  toggleData() {
+    if (this.showData) {
+      this.showData = false;
+    } else {
+      this.fetchData();
+      this.showData = true;
+    }
+  }
+
+  fetchData() {
+    this.http.get<any[]>('https://newmetsaapi.azurewebsites.net/api/ships/')
+      .subscribe((data: any[]) => {
+        this.data = data;
+      });
+  }
+
+  addShip() {
+    this.http.post('https://newmetsaapi.azurewebsites.net/api/ships/', {
+      name: this.shipName,
+      capacity: parseInt(this.shipCapacity, 10)
+    }).subscribe(() => {
+      this.fetchData();
+      this.shipName = '';
+      this.shipCapacity = '';
+    });
+  }
 }
